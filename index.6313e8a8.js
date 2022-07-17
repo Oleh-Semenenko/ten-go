59be3cf6ff5ad809fb8526b1f7aaaf8838b629d9
Oleh-Semenenko <103896561+Oleh-Semenenko@users.noreply.github.com>
2022-07-17 19:55:10 +0000

    Deploying to gh-pages from @ Oleh-Semenenko/ten-go@402cd699f10f1f08774e489e479e6ab38a5da392 🚀

---
+++ index.6313e8a8.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-about-open]"),closeMenuBtn:document.querySelector("[data-about-close]"),menu:document.querySelector("[data-about]")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.6313e8a8.js.map
