cd6cd2929875a5b4523ec22583ab79f8df5d3887
Your Name <eugene.sidorov@me.com>
2022-07-20 13:31:00 +0300

    add overlay

--- src/about.js
+++ src/about.js
@@ -1,16 +1,14 @@
 (() => {
-const refs = {
-openMenuBtn: document.querySelector("[data-about-open]"),
-closeMenuBtn: document.querySelector(
-"[data-about-close]"
-),
-menu: document.querySelector("[data-about]"),
-};
+  const refs = {
+    openMenuBtn: document.querySelector('[data-about-open]'),
+    closeMenuBtn: document.querySelector('[data-about-close]'),
+    menu: document.querySelector('[data-about]'),
+  };
 
-refs.openMenuBtn.addEventListener("click", toggleMenu);
-refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-refs.menu.classList.toggle("is-hidden");
-}
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
 })();
