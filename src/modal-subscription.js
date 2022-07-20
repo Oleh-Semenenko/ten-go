ef5461d8612a83e02559665d21e97973aa1e9e82
Roman Mokrushin <emptyspaceman1@gmail.com>
2022-07-20 15:47:49 +0300

    fixes subscription

--- src/modal-subscription.js
+++ src/modal-subscription.js
@@ -3,9 +3,12 @@
     openModalSubBtn: document.querySelector('[data-modal-sub-open]'),
     closeModalSubBtn: document.querySelector('[data-modal-sub-close]'),
     modalSub: document.querySelector('[data-modal-sub]'),
+    openSubModalBtn: document.querySelector('[data-sub-modal-open]'),
+
   };
 
   refs.openModalSubBtn.addEventListener('click', toggleModalSub);
+  refs.openSubModalBtn.addEventListener('click', toggleModalSub);
   refs.closeModalSubBtn.addEventListener('click', toggleModalSub);
 
   function toggleModalSub() {
